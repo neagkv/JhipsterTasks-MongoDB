f4e76b12b948b9ce04edd0d1d6b2972b1dbb15a5
neagkv <neagkv@gmail.com>
2018-06-10 11:44:12 -0400

    import from jdl studio

---
+++ src/main/java/jhipster/mongo/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package jhipster.mongo.repository;
+
+import jhipster.mongo.domain.Task;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Task entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface TaskRepository extends MongoRepository<Task, String> {
+
+}
